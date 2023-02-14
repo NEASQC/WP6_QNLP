16dd5e7c0c1edf7e141d9b8aaf6d259256b734b2
Pablo Suarez <p.suarez97@gmail.com>
2023-02-14 10:51:13 +0000

    Run experiment 001

--- neasqc_wp61/benchmarking/run_pre_alpha_benchmarking.py
+++ neasqc_wp61/benchmarking/run_pre_alpha_benchmarking.py
@@ -6,8 +6,8 @@ from pre_alpha_benchmarking import benchmarking_pipeline
 from plotting_tool import plotting_function, save_plot
 
 
-name_plot = 'plot_000'
-name_results = 'results_000'
+name_plot = 'plot_001'
+name_results = 'results_001'
 
 path_results = current_path + "/results/raw/"
 path_plot = current_path + "/results/analysed/"
@@ -15,6 +15,6 @@ path_plot = current_path + "/results/analysed/"
 x, y, y_stds = benchmarking_pipeline(30, save_results=True,
 path=path_results, name=name_results)
 figure = plotting_function([x], [y], [y_stds],
-title = 'Results pre-alpha ID 000',
+title = 'Results pre-alpha ID 001',
 xlabel = 'Optimization step', ylabel = 'Cost function')
 save_plot(figure, path_plot, name=name_plot)
