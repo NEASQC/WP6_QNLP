2603f42b97e00366d046ed0bc80150e4a9dce6d5
conor.dunne <conor.dunne@ichec.ie>
2023-04-19 15:49:30 +0100

    updates to 6_Classifiy_Quantum_MOdel.sh

--- neasqc_wp61/6_Classify_With_Quantum_Model.sh
+++ neasqc_wp61/6_Classify_With_Quantum_Model.sh
@@ -2,20 +2,12 @@
 
 echo 'This script classifies examples using quantum classifier model.'
 
-model='-'
-iterations='-'
-seed='-'
-epochs='-'
-runs='-'
-train='-'
-test='-1'
-outfile='-'
- 
-while getopts tr:te:s:m:e:r:i:o: flag
+
+while getopts t:v:s:m:e:r:i:o: flag
 do
     case "${flag}" in
-        tr) train=${OPTARG};;
-        te) test=${OPTARG};;
+        t) train=${OPTARG};;
+        v) test=${OPTARG};;
         s) seed=${OPTARG};;
         m) model=${OPTARG};;
         e) epochs=${OPTARG};;
@@ -24,27 +16,17 @@ do
         o) outfile=${OPTARG};;
     esac
 done
+echo "train: $train";
+echo "test: $test";
+echo "seed: $seed";
+echo "model: $model";
+echo "epochs: $epochs";
+echo "runs: $runs";
+echo "iterations: $iterations";
+echo "outfile: $outfile";
 
-if [[ "$train" == "-" ]] || [[ "$model" == "-" ]] || [[ "$seed" == "-" ]] || [[ "$outfile" == "-" ]]
-then
-__usage="
-Usage: $(basename $0) [OPTIONS]
-Options:
-  -tr <train file> 	   Json data file for classifier training
-  -te <test file> 	   Json data file for classifier testing
-  -o <output file>     Result file with predicted classes
-  -e <epochs>          Number of epochs of training for alpha
-  -m <model type>      Choose \"alpha\" model or \"pre_alpha\" model
-  -r <runs>		       Number of training runs for alpha
-  -s <seed>		       Seed for random parameters
-  -i <iterations>      Number of optimiser iterations for pre_alpha
-"
-	echo "$__usage"
+if [ "$model"=="alpha" ]; then
+python3.10 ./data/data_processing/use_alpha.py -s ${seed} -e ${epochs} -r ${runs} -tr ${train} -te ${test} -o ${outfile}
 else
-    if [["$model" == "alpha"]]
-    then
-    python ./data/data_processing/use_alpha.py -s "${seed}" -e "${epochs}" -r "${runs}" -tr "${train}" -te "${test}" -o "${outfile}"
-    else
-    python ./data/data_processing/use_pre_alpha.py -s "${seed}" -i "${iterations}" -tr "${train}" -te "${test}" -o "${outfile}"
-    fi
+python3.10 ./data/data_processing/use_pre_alpha.py -s ${seed} -i ${iterations} -tr ${train} -te ${test} -o ${outfile}
 fi
