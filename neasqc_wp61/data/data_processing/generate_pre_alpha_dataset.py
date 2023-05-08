1e1cfd902ad51d4519914b2c62ce7f7b0ba74d28
Pablo Suarez <p.suarez97@gmail.com>
2023-05-08 15:04:38 +0100

    Add script for generating the pre alpha dataset

---
+++ neasqc_wp61/data/data_processing/generate_pre_alpha_dataset.py
@@ -0,0 +1,15 @@
+from Pre_alpha_dataset import *
+import os
+current_path = os.path.dirname(os.path.abspath(__file__))
+import numpy as np
+
+directory = current_path + "/../datasets/amazonreview_train_filtered.tsv"
+train_datasets = [] 
+test_datasets = []
+seed = 364
+dataset = Pre_alpha_dataset(directory)
+dataset.generate_train_indexes(seed, 4000, False)
+dataset.generate_test_indexes(seed, 700, False)
+dataset.save_train_test_datasets(
+    current_path + "/../datasets", "amazonreview_pre_alpha"
+)
