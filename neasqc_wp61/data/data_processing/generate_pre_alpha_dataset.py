7eccb842fc5d2f57f9e0fa55a9e61f02e7e0804e
Pablo Suarez <p.suarez97@gmail.com>
2023-06-22 16:34:43 +0100

    Modify generate_pre_alpha_dataset file

--- neasqc_wp61/data/data_processing/generate_pre_alpha_dataset.py
+++ neasqc_wp61/data/data_processing/generate_pre_alpha_dataset.py
@@ -2,14 +2,25 @@ from Pre_alpha_dataset import *
 import os
 current_path = os.path.dirname(os.path.abspath(__file__))
 import numpy as np
+import argparse
 
-directory = current_path + "/../datasets/amazonreview_train_filtered.tsv"
-train_datasets = [] 
-test_datasets = []
-seed = 364
-dataset = Pre_alpha_dataset(directory)
-dataset.generate_train_indexes(seed, 4000, False)
-dataset.generate_test_indexes(seed, 700, False)
-dataset.save_train_test_datasets(
-    current_path + "/../datasets", "reduced_amazonreview_pre_alpha"
-)
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", help = "Dataset path")
+    parser.add_argument("-s", "--seed", help = "Seed for generating the dataset")
+    parser.add_argument("-tr", "--train", help = "Number of sentences in train")
+    parser.add_argument("-te", "--test", help = "Number of sentences in test")
+    parser.add_argument("-n", "--name", help = "Name of the output dataset")
+    parser.add_argument("-o", "--output", help = "Path of the output dataset")
+    args = parser.parse_args()
+
+    dataset = Pre_alpha_dataset(args.dataset)
+    dataset.generate_train_indexes(args.seed, int(args.train), False)
+    dataset.generate_test_indexes(args.seed, int(args.test), False)
+    dataset.save_train_test_datasets(
+        args.output, args.name
+    )
+
+if __name__ == "__main__":
+    main()
