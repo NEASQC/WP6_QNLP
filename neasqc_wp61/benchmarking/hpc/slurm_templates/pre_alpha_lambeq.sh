3f71f5fd7140cf92b49a5b3f686347d7a9f89b71
pablo-lauret <pablo.lauret@yahoo.es>
2023-08-09 12:01:32 +0100

    Adds a flag for batch size to the pre-alpha-lambeq SLURM template.

--- neasqc_wp61/benchmarking/hpc/slurm_templates/pre_alpha_lambeq.sh
+++ neasqc_wp61/benchmarking/hpc/slurm_templates/pre_alpha_lambeq.sh
@@ -5,13 +5,13 @@
 #SBATCH -p GpuQ
 #SBATCH -N 1
 #SBATCH -t 01:00:00
-#SBATCH --job-name=pre_alpha_lambeq_{{ s }}_{{ p }}_{{ i }}_{{ r }}_{{ an }}_{{ qn }}_{{ nl }}_{{ np }}
+#SBATCH --job-name=pre_alpha_lambeq_{{ s }}_{{ p }}_{{ i }}_{{ r }}_{{ an }}_{{ qn }}_{{ nl }}_{{ np }}_{{ b }}
    
 # Charge job to my project 
 #SBATCH -A iccom018c
 
 # Write stdout+stderr to file
-#SBATCH -o slurm_output/pre_alpha_lambeq_{{ s }}_{{ p }}_{{ i }}_{{ r }}_{{ an }}_{{ qn }}_{{ nl }}_{{ np }}.txt
+#SBATCH -o slurm_output/pre_alpha_lambeq_{{ s }}_{{ p }}_{{ i }}_{{ r }}_{{ an }}_{{ qn }}_{{ nl }}_{{ np }}_{{ b }}.txt
 
 # Mail me on job start & end
 #SBATCH --mail-user=pablo.suarez@ichec.ie
@@ -33,6 +33,7 @@ module load cuda/11.4
 # -r : number of runs of the model
 # -i : number of iterations of the optmiser
 # -p : choice of optimiser
+# -b : batch size for GPU processing 
 # -a : choice of ansatz
 # -q : number of qubits per noun type
 # -n: number of layers in the quantum circuit
@@ -41,6 +42,6 @@ module load cuda/11.4
 
 echo "`date +%T`"
 
-bash 6_Classify_With_Quantum_Model.sh -m pre_alpha_lambeq -t ./data/datasets/reduced_amazonreview_pre_alpha_train.tsv -v ./data/datasets/reduced_amazonreview_pre_alpha_test.tsv -s {{ s }} -p {{ p }}  -i {{ i }} -r {{ r }} -a {{ an }} -q {{ qn }} -n {{ nl }} -x {{ np }} -o ./benchmarking/results/raw/
+bash 6_Classify_With_Quantum_Model.sh -m pre_alpha_lambeq -t ./data/datasets/reduced_amazonreview_pre_alpha_train.tsv -v ./data/datasets/reduced_amazonreview_pre_alpha_test.tsv -s {{ s }} -p {{ p }} -i {{ i }} -r {{ r }} -a {{ an }} -q {{ qn }} -n {{ nl }} -x {{ np }} -b {{ b }} -o ./benchmarking/results/raw/
 
 echo "`date +%T`"
