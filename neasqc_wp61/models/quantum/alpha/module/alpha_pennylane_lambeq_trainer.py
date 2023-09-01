Bug fixes gradient issue alpha_lambeq models + shuffle=False for valid datasets

neasqc_wp61/models/quantum/alpha/module/alpha_pennylane_lambeq_trainer.py
@@ -59,6 +59,7 @@ class Alpha_pennylane_lambeq_trainer():
 
 
         # initialise datasets and optimizers as in PyTorch
+        # Shuffle is set to False for the validation dataset because in the predict function we need to keep the order of the predictions
         if version_original:
             self.train_dataset = Dataset(list(zip(train_circuits, self.X_train['sentence_vectorized'].values.tolist())),
                                     self.Y_train,
@@ -66,7 +67,8 @@ class Alpha_pennylane_lambeq_trainer():
 
             self.valid_dataset = Dataset(list(zip(val_circuits, self.X_test['sentence_vectorized'].values.tolist())),
                                     self.Y_test,
-                                    batch_size=self.batch_size)
+                                    batch_size=self.batch_size,
+                                    shuffle=False)
 
         else:
             self.train_dataset = Dataset(list(zip(train_circuits, np.vstack(self.X_train['sentence_embedding'].apply(np.array)))),
@@ -75,7 +77,8 @@ class Alpha_pennylane_lambeq_trainer():
 
             self.valid_dataset = Dataset(list(zip(val_circuits, np.vstack(self.X_test['sentence_embedding'].apply(np.array)))),
                                     self.Y_test,
-                                    batch_size=self.batch_size)
+                                    batch_size=self.batch_size,
+                                    shuffle=False)
         
 
         # initialise model
@@ -84,7 +87,6 @@ class Alpha_pennylane_lambeq_trainer():
         # initialise our model by pas sing in the diagrams, so that we have trainable parameters for each token
         self.model.initialise_weights()
         self.model.update_pre_qc_output_size()
-        #self.model.find_param_indexes_from_diagram_dict(self.all_circuits)
         self.model = self.model.double()
 
         # initialise loss and optimizer
@@ -119,11 +121,7 @@ class Alpha_pennylane_lambeq_trainer():
             for input, labels in self.train_dataset:
                 batch_size_ = len(input)
                 circuits, embeddings = np.array(input).T
-
-
                 self.optimizer.zero_grad()
-
-                import pdb; pdb.set_trace()
 
                 if self.version_original:
                     # Word version
@@ -132,52 +130,17 @@ class Alpha_pennylane_lambeq_trainer():
                 else:
                     # Sentence version
                     embeddings_tensor = torch.stack([torch.tensor(embedding) for embedding in embeddings])
-                    embeddings_tensor.requires_grad = True
-                    embeddings_tensor.retain_grad()
-
-                    #embeddings_tensor = torch.from_numpy(np.vstack(embeddings))
-                    predicted, embedding_pre_qc = self.model(circuits, embeddings_tensor)
+                    predicted = self.model(circuits, embeddings_tensor)
 
                 # use BCELoss as our outputs are probabilities, and labels are binary
                 loss = self.criterion(torch.flatten(predicted), torch.DoubleTensor(labels))
-                #running_loss += loss.item()
                 running_loss += loss.item()*batch_size_
                 loss.backward()
 
-                print(embedding_pre_qc.grad)
-
-
-                #print('--- embeddings_tensor GRAD ----')
-
-                # for name, param in self.model.named_parameters():
-                #     if param.grad is not None:
-                #         print(name, param.grad.sum(), param.requires_grad)
-                #     else:
-                #         print(name, param.grad, param.requires_grad)
-
-                #print weights pre_qc layer
-                print('--- PRE QC WEIGHTS ---')
-                print(self.model.pre_qc[0].weight)
-
-
                 self.optimizer.step()
-
 
                 batch_corrects = (torch.round(torch.flatten(predicted)) == torch.DoubleTensor(labels)).sum().item()
                 running_corrects += batch_corrects
-                
-
-            print("--- MODEL GRAD VALUES ---")
-            for name, param in self.model.named_parameters():
-                if param.grad is not None:
-                    print(name, param.grad.sum(), param.requires_grad)
-                else:
-                    print(name, param.grad, param.requires_grad)
-
-            
-            #print(list(self.model.parameters()))
-            #for param in self.model.parameters():
-            #    print(param.grad.data.sum())
 
             # Print epoch results
             train_loss = running_loss / len(self.train_dataset)
@@ -203,7 +166,8 @@ class Alpha_pennylane_lambeq_trainer():
                         predicted = self.model(circuits, embedding_list)
                     else:
                         # Sentence version
-                        predicted = self.model(circuits, torch.from_numpy(np.vstack(embeddings)))
+                        embeddings_tensor = torch.stack([torch.tensor(embedding) for embedding in embeddings])
+                        predicted = self.model(circuits, embeddings_tensor)
 
                     loss = self.criterion(torch.flatten(predicted), torch.DoubleTensor(labels))
                     running_loss += loss.item()*batch_size_
@@ -250,7 +214,8 @@ class Alpha_pennylane_lambeq_trainer():
                     predicted = self.model(circuits, embedding_list)
                 else:
                     # Sentence version
-                    predicted = self.model(circuits, torch.from_numpy(np.vstack(embeddings)))
+                    embeddings_tensor = torch.stack([torch.tensor(embedding) for embedding in embeddings])
+                    predicted = self.model(circuits, embeddings_tensor)
 
                 prediction_list = torch.cat((prediction_list, torch.round(torch.flatten(predicted))))
 