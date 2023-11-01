bd4023b66b1aea4721aabe35a4e88ac065180f2b
pablo-lauret <pablo.lauret@yahoo.es>
2023-11-01 17:04:51 +0000

    Adds PCA dimension parameter.

--- neasqc_wp61/6_Beta.sh
+++ neasqc_wp61/6_Beta.sh
@@ -3,11 +3,12 @@
 echo 'This script classifies examples using beta_1 model.'
 
 
-while getopts "t:v:k:o:" flag
+while getopts "t:v:d:k:o:" flag
 do
     case "${flag}" in
         t) train=${OPTARG};;
         v) test=${OPTARG};;
+        d) dimension=${OPTARG};;
         k) IFS=' ' read -r -a k <<< "${OPTARG}";;
         o) output=${OPTARG};;
     esac
@@ -19,4 +20,4 @@ echo "K values for KNN algorithm: ${k[*]}";
 echo "output: $output";
 
 echo "running beta_1"
-python3.10 ./data/data_processing/use_beta_1.py -tr ${train} -te ${test} -k ${k[*]} -o ${output}
+python3.10 ./data/data_processing/use_beta_1.py -tr ${train} -te ${test} -d ${dimension} -k ${k[*]} -o ${output}s
