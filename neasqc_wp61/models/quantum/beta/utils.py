c61d371f2b7e78ffdddbddd5d7de8a19a7736952
Pablo Suarez <p.suarez97@gmail.com>
2024-02-02 14:19:56 +0000

    Update utils file

--- neasqc_wp61/models/quantum/beta/utils.py
+++ neasqc_wp61/models/quantum/beta/utils.py
@@ -41,50 +41,6 @@ def reduce_dimension_list_of_vectors(
     pca = PCA(n_components=out_dimension)
     return pca.fit_transform(X)
 
-def normalise_list_of_vectors(X : list[np.array]) -> list[np.array]:
-    """
-    Normalises a list of vectors so that the sum
-    of its squared elements  is equal to 1.
-
-    Parameters
-    ----------
-    X : list[np.array]
-        List of vectors to be normalised
-
-    Returns
-    -------
-    X_normalised : np.array
-        List of normalised vectors
-    """
-    X_normalised = []
-    for sample in X:
-        X_normalised.append(sample/np.linalg.norm(sample))
-    return X_normalised
-
-def pad_list_of_vectors_with_zeros(X : list[np.array]) -> list[np.array]:
-    """
-    For a given list of vectors, it pads with zeros until 
-    so that the length of the vector is a power of 2
-    
-    Parameters
-    ----------
-    X : list[np.array]
-        List of vectors to be padded with zeros
-
-    Returns
-    -------
-    X_padded : list[np.array]
-        List of padded vectors
-    """
-    n = len(X[0])
-    X_padded = []
-    next_power_2 = 2 ** int(np.ceil(np.log2(n)))
-    zero_padding = np.zeros(next_power_2 - n)
-
-    for sample in X:
-        X_padded.append(np.concatenate((sample, zero_padding)))
-    return X_padded
-
 def load_data_pipeline(
     dataset_path, out_dimension
 ):
