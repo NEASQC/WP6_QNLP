37b2eed1aa3ea5dbd4c42f0148bf5eaa717ceb1a
Pablo Suarez <p.suarez97@gmail.com>
2024-02-02 14:44:42 +0000

    Update beta_1.py model

--- neasqc_wp61/models/quantum/beta/beta_1.py
+++ neasqc_wp61/models/quantum/beta/beta_1.py
@@ -1,11 +1,7 @@
 from quantum_distance import QuantumDistance as qd
-import pandas as pd 
 import pickle
-import json
 from collections import Counter
 import numpy as np
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
 import time
 import os
 import random as rd 
@@ -19,8 +15,8 @@ class QuantumKNearestNeighbours:
     """
     
     def __init__(
-            self, X_train, X_test, y_train, y_test, k_values : list[int]
-        )-> None:
+        self, X_train, X_test, y_train, y_test, k_values : list[int]
+    )-> None:
         """
         Initialises the class. 
 
@@ -44,117 +40,7 @@ class QuantumKNearestNeighbours:
         self.train_vectors = X_train
         self.test_vectors = X_test
         self.k_values = k_values
-    
-    @staticmethod
-    def load_labels(
-            train_dataset_path : str,
-            test_dataset_path : str, 
-            pca_dimension :int
-    ):
-        """
-        Loads the chosen dataset as pandas dataframe.
-
-        Parameters
-        ----------
-        train_dataset_path : str
-            Path of the train dataset
-        test_dataset_path : str
-            Path of the test dataset
-        pca_dimension : 
-            PCA dimension to make the reduction
-
-        Returns
-        -------
-        labels: list[int]
-            List with the labels of the dataset.
-            0 False, and 1 True
-        """
-
-        df_train = pd.read_csv(train_dataset_path)
-        df_test = pd.read_csv(test_dataset_path)
-
-
-        df_train['sentence_embedding'] = np.array([np.fromstring(embedding.strip(' []'), sep=',') for embedding in df_train['sentence_embedding']]).tolist()
-        df_test['sentence_embedding'] = np.array([np.fromstring(embedding.strip(' []'), sep=',') for embedding in df_test['sentence_embedding']]).tolist()
-
-        #We reduce the dimension of the sentence embedding to a 2D vector
-        ############################################################
-        # Convert the "sentence_embedding" column to a 2D NumPy array
-        X_train = np.array(
-            [embedding for embedding in df_train['sentence_embedding']])
-        X_test = np.array(
-            [embedding for embedding in df_test['sentence_embedding']])
-
-        # Initialize and fit the PCA model
-        pca = PCA(n_components=pca_dimension)  # Specify the desired number of components
-
-        pca.fit(X_train)
-        print('PCA explained variance:', pca.explained_variance_ratio_.sum())
-
-        # Transform the data to the reduced dimension for both training and test sets
-        reduced_embeddings_train = pca.transform(X_train)
-        reduced_embeddings_test = pca.transform(X_test)
-
-        # Update the DataFrames with the reduced embeddings
-        df_train['sentence_embedding'] = list(reduced_embeddings_train)
-        df_test['sentence_embedding'] = list(reduced_embeddings_test)
-
-        #Preprocess labels
-        label_encoder = preprocessing.LabelEncoder()
-        label_encoder.fit(df_train['class'])
-
-        df_train['class'] = label_encoder.transform(df_train['class'])
-        df_test['class'] = label_encoder.transform(df_test['class'])
-
-        X_train, y_train, X_test, y_test = reduced_embeddings_train, df_train['class'], reduced_embeddings_test, df_test['class']
         
-        return X_train, X_test, y_train.values, y_test.values
-    
-    @staticmethod
-    def normalise_vector(X : list[np.array]) -> list[np.array]:
-        """
-        Normalises a vector so that the sum
-        of its squared elements  is equal to 1.
-
-        Parameters
-        ----------
-        X : np.array
-            List of vectors to be normalised
-
-        Returns
-        -------
-        X_normalised : np.array
-            List of normalised vectors
-        """
-        X_normalised = []
-        for sample in X:
-            X_normalised.append(sample/np.linalg.norm(sample))
-        return np.array(X_normalised)
-    
-    @staticmethod
-    def pad_zeros_vector(X : list[np.array]) -> list[np.array]:
-        """
-        Pads a vector with zeros when its length is not a power of 2.
-
-        Parameters
-        ----------
-        X : np.array
-            List of vectors to be padded with zeros
-
-        Returns
-        -------
-        X_padded : np.array
-            List of padded vectors
-        """
-        n = len(X[0])
-        X_padded = []
-        next_power_2 = 2 ** int(np.ceil(np.log2(n)))
-        zero_padding = np.zeros(next_power_2 - n)
-
-        for sample in X:
-            X_padded.append(np.concatenate((sample, zero_padding)))
-        return X_padded
-    
     def compute_predictions(
         self, compute_checkpoints : bool = False,
         ) -> None:
@@ -181,6 +67,7 @@ class QuantumKNearestNeighbours:
                     pickle.dump(self.predictions, file)
             t2 = time.time()
             print('Time to do the iteration : ', t2 - t1)
+    
     def get_predictions(self):
         """
         Getter for the predictions
