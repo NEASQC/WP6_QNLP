8bebedc9bff31f988ad92e5b663b97197d829a73
Pablo Suarez <p.suarez97@gmail.com>
2024-02-07 15:44:09 +0000

    Fix order of imports and modify docstrings for the tests.

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -1,34 +1,40 @@
-import unittest
+import argparse
+import math as mt 
 import os 
 import sys 
-import math as mt 
-import argparse
+import unittest
 
 import pennylane as qml
 import torch
 
+# The two lines below will be removed when converting the library to a package.
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path + "/../neasqc_wp61/models/quantum/")
-from circuit import *
+import circuit as circ
 
 class TestCircuit(unittest.TestCase):
-    
+    """
+    Class for testing Circuit class.
+    """
     @classmethod
-    def setUpClass(cls) -> None:
+    def setUpClass(cls)-> None:
         """
-        Set up the class for testing
+        Set up the class for testing.
         """
         torch.manual_seed(args.seed)
         cls.n_qubits = args.n_qubits
         cls.n_layers = args.n_layers
         cls.axis_encoding = args.axis_encoding
-        if args.observable == 'X':
-            cls.observables = [qml.PauliX for _ in range(args.n_qubits)]
-        elif args.observable == 'Y':
-            cls.observables = [qml.PauliY for _ in range(args.n_qubits)]
-        elif args.observable == 'Z':
-            cls.observables = [qml.PauliZ for _ in range(args.n_qubits)]
-        cls.ansatze = [Sim14, Sim15, StronglyEntangling]
+        observables_funcs_dict = {
+            'X' : qml.PauliX, 
+            'Y' : qml.PauliY,
+            'Z' : qml.PauliZ
+        }
+        cls.observables = [
+            observables_funcs_dict[
+                args.observable] for _ in range(args.n_qubits)
+        ]
+        cls.ansatze = [circ.Sim14, circ.Sim15, circ.StronglyEntangling]
         cls.circuits = [
             ansatz(
                 cls.n_qubits, cls.n_layers,
@@ -41,12 +47,11 @@ class TestCircuit(unittest.TestCase):
         ]
         cls.input = torch.nn.Parameter(torch.randn(cls.n_qubits))
 
-
-    def test_circuits_output_correct_type(self) -> None:
+    def test_circuits_output_correct_type(self)-> None:
         """
         Test the that the the three different ansatze implemented 
-        work. To do so it will be asserted that the expected value obtained
-        after running the circuit is a tensor
+        work, asserting that the expected value obtained
+        after running the circuit is a tensor.
         """
         for i,circuit in enumerate(self.circuits):
             circuit_function = circuit.build_circuit_function
@@ -57,11 +62,11 @@ class TestCircuit(unittest.TestCase):
                 self.assertIsInstance(r, torch.Tensor)
 
     def test_circuit_outputs_of_different_rescaling_functions_are_different(
-            self) -> None:
+        self)-> None:
         """
-        Test the circuit outputs after applying rescaling functions on inputs,
-        ensuring that those vary when different 
-        rescaling functions are applied
+        Test the circuit output after applying rescaling functions on inputs,
+        ensuring that it varies when different 
+        rescaling functions are applied.
         """
         data_rescaling_functions = [
             lambda x : 0.5 * mt.pi * x,
@@ -84,12 +89,11 @@ class TestCircuit(unittest.TestCase):
             self.assertNotEqual(results_list[1], results_list[2])
             self.assertNotEqual(results_list[2], results_list[0])
                           
-
     def test_circuit_outputs_of_different_observables_are_different(
-            self) -> None:
+        self)-> None:
         """
-        Test the circuit outputs of different observables,
-        ensuring that those vary when different 
+        Test the circuit output of different observables,
+        ensuring that it varies when different 
         observables are measured 
         """
         operators = [
@@ -116,30 +120,30 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-nq", "--n_qubits", type = int,
-        help = "Number of qubits in the circuits",
+        help = "Number of qubits in the circuits.",
         default = 3
     )
     parser.add_argument(
         "-nl", "--n_layers", type = int,
-        help = "Number of layers in the circuit",
+        help = "Number of layers in the circuit.",
         default = 1
     )
     parser.add_argument(
         "-ax", "--axis_encoding", type = str,
-        help = "Axis for rotation encoding",
+        help = "Axis for rotation encoding. Must be in (X,Y,Z).",
         default = 'X'
     )
     parser.add_argument(
         "-ob", "--observable", type = str,
-        help = "Pauli operator for which output the expected value (X,Y,Z)",
+        help = ("Pauli operator for which output the expected value."
+        "Must be in (X,Y,Z)."),
         default = 'Z'
     )
     parser.add_argument(
         "-s", "--seed", type = str,
-        help = "Seed for random params and inputs",
+        help = "Seed for random params and inputs.",
         default = 1997
     )
     args, remaining = parser.parse_known_args()
     remaining.insert(0, sys.argv[0])
-
     unittest.main(argv=remaining)
